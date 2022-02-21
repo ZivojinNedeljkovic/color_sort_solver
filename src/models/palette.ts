8861f8ec97655da74fd98415b5c2550c7f3c0c82
ZivojinNedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2022-02-21 14:17:20 +0100

    added color

--- src/models/palette.ts
+++ src/models/palette.ts
@@ -17,78 +17,82 @@ class Palette {
     },
     {
       colorId: 'c',
-      hexColor: muiColors.purple[400],
+      hexColor: muiColors.pink[200],
     },
     {
       colorId: 'd',
-      hexColor: muiColors.deepPurple[500],
+      hexColor: muiColors.purple[400],
     },
     {
       colorId: 'e',
-      hexColor: muiColors.indigo[500],
+      hexColor: muiColors.deepPurple[500],
     },
     {
       colorId: 'f',
-      hexColor: muiColors.blue[500],
+      hexColor: muiColors.indigo[500],
     },
     {
       colorId: 'g',
-      hexColor: muiColors.lightBlue[400],
+      hexColor: muiColors.blue[500],
     },
     {
       colorId: 'h',
-      hexColor: muiColors.cyan[300],
+      hexColor: muiColors.lightBlue[400],
     },
     {
       colorId: 'i',
-      hexColor: muiColors.teal.A400,
+      hexColor: muiColors.cyan[300],
     },
     {
       colorId: 'j',
-      hexColor: muiColors.teal[500],
+      hexColor: muiColors.teal.A400,
     },
     {
       colorId: 'k',
-      hexColor: muiColors.green[500],
+      hexColor: muiColors.teal[500],
     },
     {
       colorId: 'l',
-      hexColor: muiColors.lightGreen[500],
+      hexColor: muiColors.green[500],
     },
     {
       colorId: 'm',
-      hexColor: muiColors.lime[500],
+      hexColor: muiColors.lightGreen[500],
     },
     {
       colorId: 'n',
-      hexColor: muiColors.yellow[500],
+      hexColor: muiColors.lime[500],
     },
     {
       colorId: 'o',
-      hexColor: muiColors.amber[500],
+      hexColor: muiColors.yellow[500],
     },
     {
       colorId: 'p',
-      hexColor: muiColors.orange[500],
+      hexColor: muiColors.amber[500],
     },
     {
       colorId: 'q',
-      hexColor: muiColors.deepOrange[300],
+      hexColor: muiColors.orange[500],
     },
     {
       colorId: 'r',
-      hexColor: muiColors.deepOrange[100],
+      hexColor: muiColors.deepOrange[300],
     },
     {
       colorId: 's',
-      hexColor: muiColors.brown[500],
+      hexColor: muiColors.deepOrange[100],
     },
     {
       colorId: 't',
-      hexColor: muiColors.grey[500],
+      hexColor: muiColors.brown[500],
     },
     {
       colorId: 'u',
+      hexColor: muiColors.grey[500],
+    },
+    {
+      colorId: 'v',
       hexColor: muiColors.blueGrey[500],
     },
 
